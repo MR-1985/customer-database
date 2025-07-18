a11acd62dfc1ad44f8bf655348a735cf345c45fd
MR-1985 <marco_r1985@yahoo.de>
2025-07-18 12:24:10 +0200

    style: added a search-function, to search for customers. / added a download-function for download the localstorage to save to PC. After deleting localstorage you can upload the .JSON to restore all data from all customers. / added alerts when customers where not found, or the localstorage is empty

--- customer-database/template/template.js
+++ customer-database/template/template.js
@@ -1,36 +1,23 @@
-function createHeader() {
-   let header = document.createElement("header");
-   header.className = "header";
-   header.innerHTML = "<h1 class='header-title'>Customer Database</h1>";
-   document.getElementById("body").appendChild(header);
-}
-
-function createLimitedContent(){
-    let limitedContent = document.createElement("div");
-    limitedContent.id = "limitedContent"
-    limitedContent.className = "limited-content";
-    document.getElementById("body").appendChild(limitedContent);
-}
-
-function createFooter() {
-   let footer = document.createElement("footer");
-   footer.className = "footer";
-   footer.innerHTML = "<p class='footer-text'>© 2025 Customer Database</p><p class='footer-text'>created by Marco Rößler</p>";
-   document.getElementById("body").appendChild(footer);
-}
-
-function createForm(){
-    let form = document.createElement("form");
-    form.id = "customerForm";
-    form.className = "customer-form";
-    form.innerHTML = `
+function formTemplate(){
+    return `
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" required placeholder="Enter new customer">
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required placeholder="Enter customer Email">
+    <input type="email" id="email" name="email" required placeholder="Enter new customer Email">
     <label for="phone">Phone:</label>
-    <input type="tel" id="phone" name="phone" required placeholder="Enter customer phone number">
-    <button type="submit" onsubmit="addCustomer()" id="submit-button">Add customer</button>
+    <input type="tel" id="phone" name="phone" required placeholder="Enter new customer phone number">
+    <button type="submit" id="submit-button">Add customer</button>
     `
-    document.getElementById("limitedContent").appendChild(form);
+}
+
+function searchFormTemplate(){
+    return `
+    <label for="name">Search Name:</label>
+    <input type="text" id="searchName" name="searchname" placeholder="Enter customer name">
+    <label for="email">Search Email:</label>
+    <input type="email" id="searchEmail" name="searchEmail" placeholder="Enter customer Email">
+    <label for="phone">Search Phone:</label>
+    <input type="tel" id="searchPhone" name="searchPhone" placeholder="Enter customer phone number">
+    <button type="submit" id="search-button">Search customer</button>
+    `
 }
